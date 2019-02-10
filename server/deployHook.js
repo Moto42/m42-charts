e6c7c74424dce94c8b1284cea1cc1e63bd7c0783
Glitch (m42-charts) <none>
2019-02-10 03:09:04 +0000

    Made more portable to move code into a gist.

--- server/deployHook.js
+++ server/deployHook.js
@@ -1,3 +1,16 @@
+/*
+This is a quick webhook that will pull the new code from a specified repo branch when it recieves a POST from github.
+To make this work, you will have to do the following...
+  Goto https://github.com/<your username>/<your repo>/settings/hooks/
+    Create a new webhook for the PUSH event.
+    set the 'content type' to `application/JSON`
+    create a SECRET and make it good an secure
+  set the following values as environmental variables...
+    GIT_SECRET = This should be the same as the SECRET you gave your webhook.
+    REPO_BRANCH = This should equal the name of the branch you are pulling from.
+*/
+
+
 const express = require('express');
 const crypto = require('crypto');
 const exec = require('child_process').exec;
@@ -9,15 +22,15 @@ function pullFromMaster(repoUrl) {
 
 function _post(req,res) {
   const auth = req.headers['x-hub-signature'];
-  const sig = "sha1=" + crypto.createHmac('sha1', process.env.SECRET).update(JSON.stringify(req.body)).digest('hex');
+  const sig = "sha1=" + crypto.createHmac('sha1', process.env.GIT_SECRET).update(JSON.stringify(req.body)).digest('hex');
   
   if(
-    auth !== sig &&
-    req.body.ref === "refs/heads/glitch"
-  ) res.status(401).send('Not Authorized');
+    auth !== sig) res.status(401).send('Not Authorized');
   else {
-    pullFromMaster(req.body.repository.git_url);
-    res.status(200).send("Pulling from repo");
+    if(req.body.ref !== `refs/heads/${process.env.REPO_BRANCH}`) res.status(200).send('Thankyou, but our code is on another branch.');
+    else{
+      pullFromMaster(req.body.repository.git_url);
+      res.status(200).send("Pulling from repo");}
   };
 };
 
