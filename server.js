87cd30fb073e890eddc9f2224f2d5c62f0eb2959
Moto42 <moto42@gmail.com>
2019-02-09 21:49:27 -0600

    cleaning up linter errors/warns

--- server.js
+++ server.js
@@ -8,25 +8,21 @@ const app = express();
 const bodyParser = require('body-parser');
 const deployHook = require('./server/deployHook');
 
-
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
-  extended: true
+  extended: true,
 }));
 app.use(bodyParser.json());
 
 app.use('/deployhook', deployHook);
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
 // listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
+const listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
